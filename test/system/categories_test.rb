0824b25dca94d6793316102442a3c2381d868ea8
grenzk <74033991+grenzk@users.noreply.github.com>
2022-09-22 14:42:07 +0800

    Add tests for categories

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,9 +1,48 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class CategoriesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit categories_url
-  #
-  #   assert_selector "h1", text: "Categories"
-  # end
+  setup { @category = categories(:first) }
+
+  test 'Creating a new category' do
+    visit categories_path
+    assert_selector 'h1', text: 'Categories'
+
+    click_on 'New category'
+    assert_selector 'h1', text: 'New category'
+
+    fill_in 'Name', with: 'Personal'
+    click_on 'Create category'
+
+    assert_selector 'h1', text: 'Categories'
+    assert_text 'Personal'
+  end
+
+  test 'Showing a category' do
+    visit categories_path
+    click_link @category.name
+
+    assert_selector 'h1', text: @category.name
+  end
+
+  test 'Updating a category' do
+    visit categories_path
+    assert_selector 'h1', text: 'Categories'
+
+    click_on 'Edit', match: :first
+    assert_selector 'h1', text: 'Edit category'
+
+    fill_in 'Name', with: 'Updated category'
+    click_on 'Update category'
+
+    assert_selector 'h1', text: 'Categories'
+    assert_text 'Updated category'
+  end
+
+  test 'Destroying a category' do
+    visit categories_path
+    assert_text @category.name
+
+    click_on 'Delete', match: :first
+    assert_no_text @category.name
+  end
 end
