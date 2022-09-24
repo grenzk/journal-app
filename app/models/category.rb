959a7b3d3802e15340390219da97114fc529d701
grenzk <74033991+grenzk@users.noreply.github.com>
2022-09-24 11:46:35 +0800

    Add real-time updates to the index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+  # after_create_commit -> { broadcast_prepend_later_to 'categories' }
+  # after_update_commit -> { broadcast_replace_later_to 'categories' }
+  # after_destroy_commit -> { broadcast_remove_to 'categories' }
+  broadcasts_to ->(category) { 'categories' }, inserts_by: :prepend
 end
