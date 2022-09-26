f1982b1c7830fc92efc25805d454cd2dc14edaef
grenzk <74033991+grenzk@users.noreply.github.com>
2022-09-26 16:54:39 +0800

    Refactor code

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -3,4 +3,8 @@ class LineItemDate < ApplicationRecord
 
   validates :date, presence: true, uniqueness: { scope: :category_id }
   scope :ordered, -> { order(date: :asc) }
+
+  def previous_date
+    category.line_item_dates.ordered.where('date < ?', date).last
+  end
 end
