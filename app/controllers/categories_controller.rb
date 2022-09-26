e03881b18a9ac06acf78838a25f62f36ee17ef0c
grenzk <74033991+grenzk@users.noreply.github.com>
2022-09-26 20:47:42 +0800

    Fix performance issue

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @line_item_dates = @category.line_item_dates.ordered
+    @line_item_dates = @category.line_item_dates.includes(:line_items).ordered
   end
 
   def new
